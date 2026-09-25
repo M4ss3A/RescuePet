4e9d328420ffc2cd1f253d80bdc103150ef167c7
Ayelen Masseroni <masseroniayelen@hotmail.com>
2026-09-25 19:40:17 -0300

    add initial role data

--- database/data.sql
+++ database/data.sql
@@ -1,1 +1,5 @@
-
+INSERT INTO roles (nombre, descripcion) VALUES
+('ADMINISTRADOR', 'Administra usuarios, permisos y configuraciones generales.'),
+('RESCATISTA_PROTECTORA', 'Registra rescates, animales y publicaciones de adopción.'),
+('HOGAR_TRANSITO', 'Informa disponibilidad y aloja animales temporalmente.'),
+('ADOPTANTE', 'Consulta publicaciones y realiza solicitudes de adopción.');
